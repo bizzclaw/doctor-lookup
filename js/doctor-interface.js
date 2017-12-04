80a0e50a0ed9286602e21dd0ab18ccf06a1d61c6
Joseph Tomlinson <bizzclaw1@gmail.com>
2017-12-04 15:53:47 -0800

    pageify and cache api results

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,44 @@
 import { Doctor } from "./../js/doctor.js";
 
 $(document).ready(function() {
-	let loadConditions = new Promise((resolve, reject) => {
-		Doctor.GetConditions(function(response) {
-			resolve(response);
-		});
-	});
 
-	loadConditions.then((response) => {
-		console.log(response);
+	let conditionPage = 0
+	let resultsPanel = $("#conditions-results")
+
+	let fillConditions = function(page) {
+		let loadConditions = new Promise((resolve, reject) => {
+			Doctor.GetConditions(page, function(response) {
+				resolve(response);
+			});
+		});
+
+		loadConditions.then((response) => {
+			$(".page-select").removeClass("disabled");
+			if (!response.data || response.data.length < 1) {
+				conditionPage -= 1;
+				return false
+			}
+
+			resultsPanel.empty();
+
+			response.data.forEach(function(condition) {
+				resultsPanel.append(`<p class="condition-list condition-available" uid=`+ condition.uid +`>` + condition.name + `</p>`);
+			});
+
+			$("condition-available").click(function() {
+
+			});
+
+			$("#pagecount").text(conditionPage + 1);
+			return true
+		});
+	}
+
+	fillConditions(0)
+
+	$(".page-select").click(function() {
+		$(".page-select").addClass("disabled", "1");
+		conditionPage = Math.max(conditionPage + parseInt($(this).attr("pagedir")),0);
+		fillConditions(conditionPage)
 	});
 });
--- js/doctor.js
+++ js/doctor.js
@@ -1,30 +1,43 @@
 const API = require("./../.env");
 const BDKEY = API.betterDoctor;
+const RESULTSPERPAGE = 20
 
 import { Location } from "./../js/location.js";
 
+let _conditionsCache = []
+
 export class Doctor {
 
-	static GetConditions(callback) {
+
+	static GetConditions(page, callback) {
+		if (_conditionsCache[page]) {
+			callback(_conditionsCache[page]); // callback with cached result instead of wasting api calls.
+			return true;
+		}
+		let skip = RESULTSPERPAGE * page;
+		let limit = RESULTSPERPAGE;
 		$.ajax({
-			url: `https://api.betterdoctor.com/2016-03-01/conditions?user_key=${BDKEY}`,
+			url: `https://api.betterdoctor.com/2016-03-01/conditions?skip=${skip}&limit=${limit}&user_key=${BDKEY}`,
 			type: "GET",
 			data: {
 				format: "json"
 			},
 			success: function(response) {
+				_conditionsCache[page] = response;
 				callback(response);
 			},
 			error: function(error) {
 				callback(false, error);
 			}
 		});
+		return false;
 	}
 
 	static FindDoctors(location, callback, radius = 200, limit = 10) {
 		let locationStr = Location.GetSearchLocation(location);
 		location.GetClientLocation(locationStr, function(userPosStr) {
 			locationStr += ", " + radius; // concat the radius after we've already used the center pos as a fallback
+			console.log(locationStr);
 			$.ajax({
 				url: `https://api.betterdoctor.com/2016-03-01/doctors?location=${locationStr}&user_location=${userPosStr}&skip=0&limit=${limit}&user_key=${BDKEY}`,
 				type: "GET",
